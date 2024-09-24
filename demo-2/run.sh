682f27b72b8dca3c37dcb49f8a595a9077fbc2d6
Harsh Bardhan Mishra <erbeusgriffincasper@gmail.com>
2024-09-24 03:59:03 +0000

    setup the lambda resizer application

---
+++ demo-2/run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+awslocal s3api create-bucket --bucket original-images
+awslocal s3api create-bucket --bucket resized-images
+
+# macOS
+docker run --platform linux/x86_64 --rm -v "$PWD":/var/task "public.ecr.aws/sam/build-python3.11" /bin/sh -c "pip3 install -r requirements.txt -t libs; exit"
+
+# Linux/Windows
+# pip3 install -r requirements.txt --platform manylinux2014_x86_64 --only-binary=:all: -t package
+
+cd libs && zip -r ../lambda.zip . && cd ..
+zip lambda.zip lambda_function.py
+
+awslocal lambda create-function \
+    --function-name ImageResizerFunction \
+    --runtime python3.11 \
+    --handler lambda_function.lambda_handler \
+    --zip-file fileb://lambda.zip \
+    --role arn:aws:iam::000000000000:role/lambda-role \
+    --timeout 60
+
+awslocal s3api put-bucket-notification-configuration \
+    --bucket original-images \
+    --notification-configuration '{
+        "LambdaFunctionConfigurations": [
+            {
+                "LambdaFunctionArn": "arn:aws:lambda:us-east-1:000000000000:function:ImageResizerFunction",
+                "Events": ["s3:ObjectCreated:*"]
+            }
+        ]
+    }'
+
+awslocal s3 cp image.png s3://original-images/image.png
+awslocal s3 cp image.jpg s3://original-images/image.jpg
+
+awslocal s3 ls s3://resized-images
+awslocal s3 ls s3://original-images
